91b376e4c767755228312b5e16646880bdc8c165
Filipa Vieira <91594330+FilipaChambel@users.noreply.github.com>
2025-02-09 18:06:54 +0000

    frontend created

--- grpc-chat/src/App.jsx
+++ grpc-chat/src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [socket, setSocket] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [receiver, setReceiver] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
+  useEffect(() => {
+    if(socket){
+      socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        if(data.type === "login_success"){
+          setLoggedIn(true);
+        } else if(data.type === "new_message"){
+          setMessages((prev) => [...prev, `${data.sender}: ${data.content}`]);
+        }
+      }
+    }
+  },[socket]);
+
+  const handleLogin = () => {
+    const ws = new WebSocket(`ws://localhost:8000/ws/${username}`);
+    ws.onopen = () => {
+      ws.send(JSON.stringify({action: "login", password}));
+    };
+    setSocket(ws);
+  };
+
+  const sendMessage = () => {
+    if(socket && receiver && message){
+      socket.send(JSON.stringify({action: "send_message", receiver, content: message}))
+      setMessage((prev) => [...prev, `You: ${message}`]);
+      setMessage("");
+    }
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{textAlign: "center", padding: "20px"}}>
+      {!loggedIn ? (
+        <div>
+          <h2>Login</h2>
+          <input placeholder='Username' onChange={(e)=> setUsername(e.target.value)}/>
+          <input type='password' placeholder='Password' onChange={(e)=> setPassword(e.target.value)}/>
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      ): (
+        <div>
+          <h2>Chat as {username}</h2>
+          <input placeholder='Receiver' onChange={(e)=> setReceiver(e.target.value)}/>
+          <input placeholder='Message' value={message} onChange={(e)=> setMessage(e.target.value)}/>
+          <button onClick={sendMessage}>Send</button>
+          <div>
+            <h3>Messages</h3>
+            {messages.map((msg, index) => (
+              <p key={index}>{msg}</p>
+            ))}
+          </div>
+        </div>
+      )}
+      
+    </div>
   )
 }
 
-export default App
